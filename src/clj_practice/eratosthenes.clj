c43efcf94f91ba1a9e0a516282df2f7d4ee98a64
John Weaver <john@pledge4code.com>
2012-06-26 16:28:35 -0700

    Address sieve performance issue.

--- src/clj_practice/eratosthenes.clj
+++ src/clj_practice/eratosthenes.clj
@@ -1,20 +1,21 @@
 (ns clj-practice.eratosthenes)
 
 
-(defn clear-multiples [number-array current-number]
-  (amap ^booleans number-array idx ret
-        (and (aget number-array idx)
-             (not (and (aget number-array idx) (> (+ 3 idx) current-number)  (= (mod (+ idx 3) current-number) 0))))))
+(defn clear-multiples [^booleans number-array current-number]
+  (let [ret (aclone number-array)]
+    (doseq [idx (range (- (* 2 current-number) 3) (alength number-array) current-number)]
+      (aset ret idx false))
+    ret))
 
 
-(defn sieve-filter [number-array current-number limit]
+(defn sieve-filter [^booleans number-array current-number limit]
   (if (< current-number limit)
     (sieve-filter (if (aget number-array (- current-number 3))
                     (clear-multiples number-array current-number)
                     number-array) (+ 1 current-number) limit)
     number-array))
 
-(defn map-back-to-numbers [number-array maximum]
+(defn map-back-to-numbers [^booleans number-array maximum]
   (keep-indexed (fn [idx itm] (if (aget number-array idx) itm nil)) (range 3 (+ 1 maximum))))
 
 (defn sieve [maximum]
